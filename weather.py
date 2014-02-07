676506e99eb681315fb0ca5aeec1c6de7bf8b52a
jesusba <jesusba60@gmail.com>
2014-02-07 20:28:26 +0100

    Archivo PYTHON finalizado

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@
 import requests
 import json
 
-ciudades = {"1":"Almeria","2":"Cadiz","3":"Cordoba","4":"Granada","5":"Huelva","6":"Jaen","7":"Malaga","8":"Sevilla"}
+ciudades = {"1":"Almería","2":"Cádiz","3":"Córdoba","4":"Granada","5":"Huelva","6":"Jaén","7":"Málaga","8":"Sevilla","9":"Dos Hermanas"}
 
 print """
 1. Almería
@@ -13,10 +13,10 @@ print """
 6. Jaén
 7. Málaga
 8. Sevilla
+9. Dos Hermanas
 		"""
 
 peticion = raw_input("¿De qué ciudad quieres saber el tiempo?: ")
-#print ciudades[peticion]
 
 respuesta = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % ciudades[peticion]})
 
@@ -24,6 +24,6 @@ dicc = json.loads(respuesta.text)
 
 temperatura = dicc["main"]["temp"]
 
-temperaturareal = temperatura - 273
+tempreal = temperatura - 273
 
-print ,% temperaturareal
+print "La temperatura actual de %s es %s grados centígrados" % (ciudades[peticion],tempreal)
